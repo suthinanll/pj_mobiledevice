3980f3ec465c120ad6402da86640b9c072df07c6
jakkare01 <jakkaret.c@kkumail.com>
2025-02-28 01:15:07 +0800

    Admin Dashboard

--- app/src/main/java/com/example/ass07/admin/PetAPI.kt
+++ app/src/main/java/com/example/ass07/admin/PetAPI.kt
@@ -1,21 +1,11 @@
 package com.example.ass07.admin
 
-import retrofit2.http.GET
-
-import com.example.ass07.customer.Mypet.AddPetTypeResponse
 import com.example.ass07.customer.Mypet.PetType
-import com.example.ass07.customer.Mypet.UpdatePetRequest
 import com.example.ass07.customer.Mypet.petMember
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.Body
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
-
-import retrofit2.http.POST
-import retrofit2.http.PUT
-import retrofit2.http.Path
+import retrofit2.http.GET
 
 interface PetApi {
     @GET("getPetTypes")
